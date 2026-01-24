fcf294a64fec671fbb4f5d227156264b6ba898f5
seb-creator01 <precioussebastian70@gmail.com>
2026-01-24 13:54:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
 const CACHE_NAME = 'xox-arena-v1';
-const assets = ['./', './index.html'];
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Install event: Caches the game files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Fetch event: Allows the app to work offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
